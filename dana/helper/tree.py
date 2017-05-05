7c03d1a9a84342539d8e4db314af6ef8190a18d1
etsal <emiltsal@yahoo.gr>
2017-05-05 23:01:25 +0000

    removed is_terminal attribute from Node

--- dana/helper/tree.py
+++ dana/helper/tree.py
@@ -7,11 +7,9 @@ class Node(object):
         self.children = deque() 
         for child in args:
             self.children.append(child)
-        self.is_terminal = len(self.children) == 0 
         self.value = kwargs.get("value", None)
 
     def __str__(self):
-        result = ("TERMINAL " if self.is_terminal else "") + self.name
         result = self.name
         if self.value is not None:
             result += ", Value:{}".format(self.value)
