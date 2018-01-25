b6b8d85159e194f0f17122840041c233d9f3fdb9
etsal <emiltsal@yahoo.gr>
2018-01-25 11:53:35 +0000

    refactored codegen/block.py

--- dana/compiler/codegen/block.py
+++ dana/compiler/codegen/block.py
@@ -1,7 +1,28 @@
+""" 
+    IR Block Creation module. The goal of each of the four functions
+is to produce self-contained series blocks, with the exception of 
+terminators to be backpatched. 
+
+Arguments:
+func -- The IR function, used for its .append_basic_block() method
+block -- The semantic block to be turned into a sequence of IR blocks
+table -- A table with instructions to be backpatched
+
+Return:
+blks -- A sequence of IR blocks
+"""
+
 from compiler.codegen.expr import irgen_expr as irgen_expr
 from compiler.codegen.stmt import irgen_stmt as irgen_stmt
 from llvmlite import ir as ir
 
+def mark_backpatch(builder, table):
+    instr = builder.unreachable() 
+    table[instr] = (builder.block, None)
+
+def backpatch(block, instr):
+    block.replace(block.terminator, instr)
+    block.terminator = instr 
 
 def irgen_block(func, block, table):
     irgen = dict({"if" : irgen_if,
@@ -19,21 +40,28 @@ def irgen_if(func, block, table):
     """Generate the blocks representing an if-elif*-else? statement"""
     start = func.append_basic_block()
     builder = ir.IRBuilder(start)
+
+
     pred = irgen_expr(block.cond, builder, table)
-
+    
+    # WIll be patched with a conditional branch to the two paths
     temp = builder.unreachable()    
-    if_blks = irgen_block(func, block.if_path, table)
 
+    # The two paths, if there is no else path 
+    # we create a stub one to be optimized out by LLVM
+    if_blks = irgen_block(func, block.if_path, table)
     else_blks = []
     if block.else_path:
+        # Elif chains are here if they exist
         else_blks += irgen_block(func, block.else_path, table)
     else:
         else_blks = [func.append_basic_block()]
         else_builder = ir.IRBuilder(else_blks[0])
-        tmp = else_builder.unreachable() 
-        table.nexts[tmp] = (builder.block, None)
+        mark_backpatch(else_builder, table.nexts)
 
-    start.replace(temp, ir.ConditionalBranch(start, "br", [pred, if_blks[0], else_blks[0]]))
+    branch = ir.ConditionalBranch(start, "br", [pred, if_blks[0], else_blks[0]])
+    start.replace(temp, branch)
+    start.terminator = branch 
     return [start] + if_blks + else_blks 
 
 
@@ -47,24 +75,29 @@ def irgen_loop(func, block, table):
 
     # In case the last block is has an instruction to be patched,
     # we patch it with a jump to the entry block
-    if isinstance(exit.instructions[-1], ir.Unreachable) and \
-        exit.instructions[-1] in table.nexts:
-        exit.replace(exit.instructions[-1], ir.Branch(exit, "br", [entry])) 
+    if exit.terminator in table.nexts:
+        branch = ir.Branch(exit, "br", [entry])
+        backpatch(exit, branch)
 
+    # Backpatching 
     for blk in blks:
-        for inst in blk.instructions:
-            if inst in table.breaks:
-                (b, lab) = table.breaks[inst]
-                if not lab or (block.label and lab == block.label):
-                    table.breaks.pop(inst)
-                    table.nexts[inst] = (b, lab)
+        term= blk.terminator
+        # Breaks out of the current loop are nexts 
+        # to the basic block following it 
+        if term in table.breaks:
+            (b, lab) = table.breaks[term]
+            if not lab or (block.label and lab == block.label):
+                table.breaks.pop(term)
+                table.nexts[term] = (b, lab)
 
-            if inst in table.conts:
-                (b, lab) = table.conts[inst]
-                if not lab or (block.label and lab == block.label):
-                    table.conts.pop(inst)
-                    b.replace(inst, ir.Branch(exit, "br", [entry])) 
-                    
+        # Conts on the current loop can be backpatched immediately 
+        if term in table.conts:
+            (b, lab) = table.conts[term]
+            if not lab or (block.label and lab == block.label):
+                table.conts.pop(term)
+                branch = ir.Branch(exit, "br", [entry])
+                backpatch(b, branch)
+                b.terminator = branch 
 
     return blks 
 
@@ -78,15 +111,10 @@ def irgen_basic(func, block, table):
     for stmt in block.stmts:
         irgen_stmt(stmt, builder, table)
             
-    # If a block is not explicitly terminated (with a 
-    # terminator instruction), we put a placeholder
-    # to be backpatched into a branch to the next instruction,
-    # thus satisfying LLVM IR constraints (all basic blocks end
-    # with a terminator, and all jumps are done to the _beginning_
-    # of a basic block)
+    # Placeholder to be backpatched into a branch to the next instruction,
+    # thus satisfying LLVM IR constraints about terminators and basic blocks
     if not blk.is_terminated:
-        tmp = builder.unreachable() 
-        table.nexts[tmp] = (builder.block, None)
+        mark_backpatch(builder, table.nexts)
 
     return [blk]
    
@@ -99,9 +127,12 @@ def irgen_container(func, block, table):
     for child in block.children:
         previous = children
         children = irgen_block(func, child, table)
+        # Backpatch "next" undefined instructions 
+        # We check all blocks, because if-else and loops can have multiple next's 
         for blk in previous:
-            if blk.instructions[-1] in table.nexts:
-                blk.replace(blk.instructions[-1], ir.Branch(blk, "br", [children[0]]))
+            if blk.terminator in table.nexts:
+                branch = ir.Branch(blk, "br", [children[0]])
+                backpatch(blk, branch)
 
         blks += children 
 
