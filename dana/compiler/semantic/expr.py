19a5d44c57aa8f4db06a737e56bd62f8d98d9f41
etsal <emiltsal@yahoo.gr>
2018-01-28 17:35:49 +0000

    cleaned up semantic/expr.py

--- dana/compiler/semantic/expr.py
+++ dana/compiler/semantic/expr.py
@@ -10,7 +10,8 @@ class DanaExpr(object):
     @classmethod
     def factory(self, d_expr, table):
 
-        # Because the parsing rule is cond : expr | xcond
+        # Because the parsing rule is cond : expr | xcond,
+        # we get the child and work we that instead
         if d_expr.name == "p_cond":
             d_expr = d_expr.children[0]
 
@@ -22,10 +23,10 @@ class DanaExpr(object):
         if d_func_call:
             return DanaCall(d_func_call, table)
 
+        # Lvalues are also represented by DanaExpr, so
+        # because of the rule stmt : lvalue ":=" expr, 
+        # d_expr might not actually be an expr
         d_lvalue = d_expr.find_first_child("p_lvalue")
-        # Small hack: lvalues are also represented by DanaExpr, so
-        # because of the rule stmt : lvalue ":=" expr d_expr might
-        # not actually be an expr
         if d_expr.name == "p_lvalue":
             d_lvalue = d_expr
 
@@ -69,10 +70,6 @@ class DanaConst(DanaExpr):
         value = const.value
         if const.value in ["true", "false"]:
             value = 1 if const.value == "true" else 0
-        elif isinstance(value, int):
-            value = const.value
-        else:
-            value = ord(ast.literal_eval(const.value)[0])
 
         dtype = DanaType("int") if const.name == "p_number" else DanaType("byte")
         self._set_attributes([], dtype, "const", value=value)
@@ -82,28 +79,23 @@ class DanaCall(DanaExpr):
     def __init__(self, d_func_call, table):
         super().__init__()
         name = d_func_call.find_first_child("p_name").value
-        check_scope(d_func_call.linespan, name, table)
 
-        if not table[name].is_function():
-            print("Lines {}: Nonfunction called as function", d_func_call.linespan, name)
-            raise DanaTypeError
-
-        expected_args = table[name].args
         d_exprs = d_func_call.find_all("p_expr")
         exprs = [DanaExpr.factory(d_expr, table) for d_expr in d_exprs]
         types = [expr.type for expr in exprs]
-        if expected_args != types:
-            print("Lines {}: Arguments {} in function call {} instead of {} " \
-                        .format(d_func_call.linespan, \
-                                [str(t) for t in types], name, [str(t) for t in expected_args]))
 
-        dtype = DanaType(table[name].base)
-        self._set_attributes(exprs, dtype, "call", value=name)
+        check_scope(d_func_call.linespan, name, table)
+        base = table[name].base
+        dtype = DanaType(base, args=types)
+        check_type(d_func_call.linespan, table[name], dtype)
+
+        dtype_base = DanaType(base)
+        self._set_attributes(exprs, dtype_base, "call", value=name)
 
 
 class DanaUnary(DanaExpr):
     def __init__(self, operator, operand, table):
-        pairs = dict({"!" : [DanaType("byte")], 
+        optype = dict({"!" : [DanaType("byte")], 
                       "+" : [DanaType("int")],
                       "-" : [DanaType("int")],
                       "not" : [DanaType("logic"), DanaType("byte")],
@@ -118,10 +110,8 @@ class DanaUnary(DanaExpr):
 
         super().__init__()
         child = DanaExpr.factory(operand, table)
-        if operator in pairs:
-            in_types(operand.linespan, pairs[operator], child.type)
-        
-
+        if operator in optype:
+            in_types(operand.linespan, optype[operator], child.type)
 
         self._set_attributes([child], child.type, renamed[operator])
         return
@@ -137,41 +127,29 @@ class DanaBinary(DanaExpr):
                  comparison_ops + logic_ops
 
     def __init__(self, operator, arg1, arg2, table):
+        renamed = dict({"STAR" : "*", 
+                        "SLASH" : "/", 
+                        "PERCENT" : "%", 
+                        "=" : "==",
+                      })
+
         super().__init__()
         op1 = DanaExpr.factory(arg1, table)
         op2 = DanaExpr.factory(arg2, table)
-        if op1.type != op2.type:
-            print("Lines {},{}: Operands {} and {}" \
-                    .format(arg1.linespan, arg2.linespan, str(op1.type), str(op2.type)))
-            return
+        check_type(arg1.linespan, op1.type, op2.type)
 
-        # Map some operators to their symbols
-        new_names = dict({"STAR" : "*", "SLASH" : "/", "PERCENT" : "%", "=" : "==",})
+        if operator in self.binary_ops + self.comparison_ops: 
+            in_types(arg1.linespan, [DanaType("int"), DanaType("byte")], op1.type)
 
+        if operator in self.bit_ops: 
+            check_type(arg1.linespan, DanaType("byte"), op1.type)
 
-        if operator in self.binary_ops + self.comparison_ops and \
-            not op1.type in [DanaType("int"), DanaType("byte")]:
-            print("Lines {},{}: Operands {} for arithmetic operation" \
-                    .format(arg1.linespan, arg2.linespan, str(self.type)))
-            return
+        if operator in self.logic_ops: 
+            logic_types = [DanaType("byte"), DanaType("logic"), DanaType("int")]
+            check_type(arg1.linespan, logic_types, op1.type)
 
-        if operator in self.bit_ops and op1.type != DanaType("byte"):
-            print("Lines {},{}: Operands {} for byte operation" \
-                    .format(arg1.linespan, arg2.linespan, str(self.type)))
-            return
-
-
-        if operator in self.logic_ops and \
-            not op1.type in [DanaType("logic"), DanaType("byte"), DanaType("int")]:
-
-            print("Lines {},{}: Operands {} for logic operation" \
-                    .format(arg1.linespan, arg2.linespan, str(self.type)))
-
-
-        self.children += [op1, op2]
-        self.type = op1.type
-        self.operator = new_names[operator] if operator in new_names else operator
-
+        operator = renamed[operator] if operator in renamed else operator
+        self._set_attributes([op1, op2], op1.type, operator)
 
 
 
@@ -179,6 +157,7 @@ class DanaLvalue(DanaExpr):
     def __init__(self, d_lvalue, table):
         super().__init__()
 
+        # The string case is self-contained
         string = d_lvalue.find_first_child("p_string")
         if string:
             value = ast.literal_eval(string.value) + "\0"
@@ -187,29 +166,37 @@ class DanaLvalue(DanaExpr):
             return
 
 
+        # The function symbol
         d_id = d_lvalue.find_first("p_name")
         name = d_id.value
         check_scope(d_lvalue.linespan, name, table)
 
+        # Get all indices
         d_exprs = d_lvalue.find_all("p_expr")
         exprs = [DanaExpr.factory(d_expr, table) for d_expr in d_exprs]
         for expr in exprs:
             check_type(d_lvalue.linespan, expr.type, DanaType("int")) 
                     
+        # Make sure there are not too many indices
         base = table[name]
-        dims = len(base.dims) + base.pdepth
-        if dims < len(exprs):
-            raise TypeError("Invalid dereferencing")
+        total_dims = len(base.dims) + base.pdepth
+        if total_dims < len(exprs):
+            raise DanaTypeError("Invalid dereferencing")
 
-        rest = len(exprs) - base.pdepth
-        if rest >= 0:
-            dtype = DanaType(base.base, \
-                             dims=base.dims[rest:], \
-                             args=base.args)
+        # When we have both dimensions and pdepth, we have 
+        # a pointer to an array, not an array of pointers. 
+        # That influences the type of the dereferenced expression
+        dims, pdepth = (base.dims, base.pdepth)
+        if len(exprs) > base.pdepth:
+            dims = base.dims[(len(exprs) - base.pdepth):]
+            pdepth = 0 
         else:
-            dtype = DanaType(base.base, \
-                             dims=base.dims, \
-                             pdepth=(-rest), \
-                             args=base.args)
+            dims = base.dims
+            pdepth = base.pdepth - len(exprs)
+
+        dtype = DanaType(base.base, \
+                         dims=dims, \
+                         pdepth=pdepth, \
+                         args=base.args)
         
         self._set_attributes(exprs, dtype, "lvalue", value=name)
