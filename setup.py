121ad38f2a9129dc22f99902edaf1e923f39167d
etsal <aimilios.tsalapatis@gmail.com>
2018-02-11 18:32:05 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DanaCompiler",
-    version="0.9.0",
+    version="0.9.1",
     author="Aimilios Tsalapatis",
     author_email="emiltsal@yahoo.gr",
     url="",
